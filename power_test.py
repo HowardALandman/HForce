2f6a0cef43a51d1c5a99ba39602b85434dd3245f
Howard A. Landman <howard@riverrock.org>
2016-07-25 22:47:47 -0600

    Reliably log data to a file.

--- power_test.py
+++ power_test.py
@@ -11,14 +11,24 @@ n_pixels = 72	# Number of LEDs in strip
 strip   = Adafruit_DotStar(n_pixels)	# Use SPI (pins 10=MOSI, 11=SCLK)
 strip.begin()	# Initialize pins for output
 
+# Open log file.
+f = open('powerlog.txt', 'w')
+f.write("Brightness\tVoltage\n")
+
 for p in range(n_pixels):
 	strip.setPixelColor(p, WHITE)
 
 for b in range(256):
 	#print b
+	# Set the brightness.
 	strip.setBrightness(b)
 	strip.show()                     # Refresh strip
+	# Prompt user for data entry.
 	answer = raw_input(str(b)+' ')
-	#time.sleep(1.0)
+	# Log it.
+	f.write(str(b)+"\t"+answer+"\n")
+	# Wait a fraction of a second to help ensure that the write completes,
+	# in case the next brightness level crashes the system.
+	time.sleep(0.1)
 
-
+f.close()
