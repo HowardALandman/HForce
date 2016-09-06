d205a2469462b2fa6fd50d79348524210f67846c
Howard A. Landman <howard@riverrock.org>
2016-09-06 00:47:22 -0600

    Slight simplification (deleted redundant "path" variable).

--- readColorMaps.py
+++ readColorMaps.py
@@ -2,20 +2,20 @@
 # but eventually we'd like to have shifting ones as in G-Force.
 # As a first step, allow ColorMaps to be read from .png files.
 # Normally these would be 256 x 1 x 8-bit RGB.
+# By default we keep them in directory ColorMaps under the app's home.
 
 import os
 import png
 from fnmatch import filter
 
 def readColorMaps(d=os.path.join(os.getcwd(),"ColorMaps")):
-	path = d
 	all_files = []
 	all_paths = []
 	# Find all .png files in ColorMaps and its subdirectories.
-	for (path, dirs, files) in os.walk(path):
+	for (d, dirs, files) in os.walk(d):
 		all_files += files
 		for f in files:
-			all_paths += [os.path.join(path,f)]
+			all_paths += [os.path.join(d,f)]
 	png_files = filter(all_files, '*.png')
 	png_paths = filter(all_paths, '*.png')
 	colorMaps = []
